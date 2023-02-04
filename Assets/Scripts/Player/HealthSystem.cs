a829ab5ab1ec001a736b7f0ef7a1d22e5eb2b5e1
T3NSH11 <m.zaid@student.saedubai.com>
2023-02-05 00:50:45 +0400

    Health System

---
+++ Assets/Scripts/Player/HealthSystem.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    public int MaxHealth;
+    public int Health;
+    public GameObject[] Hearts;
+    public Sprite FullHeart;
+    public Sprite EmptyHeart;
+    void Start()
+    {
+        for (int i = 0; i < Hearts.Length; i++)
+        {
+            Hearts[i].GetComponent<Image>().sprite = FullHeart;
+        }
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        Hearts[Health - 1].GetComponent<Image>().sprite = EmptyHeart;
+        Health -= 1;
+    }
+}
