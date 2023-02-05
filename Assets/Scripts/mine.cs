6044b4c9ada9fd5bd018096dca0b72b158d88173
T3NSH11 <m.zaid@student.saedubai.com>
2023-02-05 16:12:11 +0400

    update mine

--- Assets/Scripts/mine.cs
+++ Assets/Scripts/mine.cs
@@ -5,7 +5,7 @@ public class mine : MonoBehaviour
 {
     public GameObject Player;
     private bool hasTakenDamage = false;
-    private Collider[] collider ; 
+    private Collider[] collider; 
     private void Awake()
     {
         collider = new Collider[1]; 
@@ -14,23 +14,21 @@ public class mine : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var collisions=Physics.OverlapBoxNonAlloc(this.transform.position, new Vector3(5, 5, .9f), collider);
-        if (collider[0] !=null)
+        var collisions = Physics.OverlapBoxNonAlloc(this.transform.position, new Vector3(5, 5, .9f), collider);
+        if (collider[0] != null)
         {
             if (!hasTakenDamage)
-            {                
+            {
                 Player.GetComponent<HealthSystem>().TakeDamage();
-                gameObject.GetComponent<Animator>().SetBool("BombHit", true);
-                hasTakenDamage = true;
+                gameObject.GetComponent<Animation>().Play();
+                Destroy(gameObject.GetComponent<MeshRenderer>());
                 Destroy(GetComponent<BoxCollider>());
+                hasTakenDamage = true;
+
                 Destroy(gameObject, 1);
             }
 
         }
-    }
-    private void OnDrawGizmos()
-    {
-        //Gizmos.DrawCube(this.transform.position, new Vector3(15, 15, 3));
     }
 }
 
