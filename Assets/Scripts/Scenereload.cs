555caee2b9c22e1f6dc8bb023a92daa4ba04ccc6
mazen320 <m.shaaar@saedubai.com>
2023-02-05 17:55:17 +0400

    Pushh

--- Assets/Scripts/Scenereload.cs
+++ Assets/Scripts/Scenereload.cs
@@ -1,12 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 
 public class Scenereload : MonoBehaviour
 {
-    public void ReloadLevel()
-    {
-        EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().name);
-    }
+
 }
